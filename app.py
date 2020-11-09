f31215f6b33456d8fd4ad104eeea6ecd5473cb36
Oleg <saga6021@gmail.com>
2020-11-09 14:53:22 +0700

    Changes №2

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
-import gunicorn
+from cur_conv import *
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    values = [check_currency(DOLLAR_RUB), check_currency(EURO_RUB)]
+    for value in values:
+        print(value)
+    return render_template("index.html", cur_value=values)
 
 if __name__ == "__main__":
     app.run()
