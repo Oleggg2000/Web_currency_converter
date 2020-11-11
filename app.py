c108950e7afa3aabf7d38bde45e03120c3146c97
Oleg <saga6021@gmail.com>
2020-11-11 10:32:06 +0700

    Changes №7

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
 from cur_conv import *
 
@@ -11,6 +11,17 @@ def index():
     for value in values:
         print(value)
     return render_template("index.html", cur_value=values)
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    values = [check_currency(DOLLAR_RUB), check_currency(EURO_RUB), check_currency(JPY_RUB)]
+    if request.method == "POST":
+        from_cur = request.form["from"]
+        amount = request.form["cash"]
+        to_cur = request.form["to"]
+        print(from_cur, amount, to_cur)
+    return render_template("index.html", cur_value=values)
+
 
 
 if __name__ == "__main__":
